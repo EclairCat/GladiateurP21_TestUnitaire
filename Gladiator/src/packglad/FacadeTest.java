5e01e94efffaf79f6f22751b9af0f76c46c64845
BOUGEDRAWI Rabie <bougedrawi@troglo.iutrs.unistra.fr>
2017-06-08 17:20:51 +0200

    facade test creer

---
+++ Gladiator/src/packglad/FacadeTest.java
@@ -0,0 +1,249 @@
+package packglad;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FacadeTest {
+    public FacadeTest() {
+    }
+
+    /**
+     * @see Facade#lancerJeu()
+     */
+    @Test
+    public void testLancerJeu() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#lancerJeuDEssai()
+     */
+    @Test
+    public void testLancerJeuDEssai() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#parametrage(Integer,Integer,Integer,Integer)
+     */
+    @Test
+    public void testParametrage() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#creerRetiaire(String,Integer,Integer)
+     */
+    @Test
+    public void testCreerRetiaire() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#creerMirmillon(String,Integer,Integer)
+     */
+    @Test
+    public void testCreerMirmillon() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerTousGladiateurs()
+     */
+    @Test
+    public void testListerTousGladiateurs() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerAgresseurs(Integer)
+     */
+    @Test
+    public void testListerAgresseurs() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#faireSaluerGladiateur(Integer)
+     */
+    @Test
+    public void testFaireSaluerGladiateur() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#faireRapport(Integer)
+     */
+    @Test
+    public void testFaireRapport() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#declarerArmes(Integer)
+     */
+    @Test
+    public void testDeclarerArmes() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#supprimerGlad(Integer)
+     */
+    @Test
+    public void testSupprimerGlad() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#creerUneArme(String,Integer,Integer)
+     */
+    @Test
+    public void testCreerUneArme() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#autoriserArmeAuxMirmillons(Integer)
+     */
+    @Test
+    public void testAutoriserArmeAuxMirmillons() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#autoriserArmeAuxRetiaires(Integer)
+     */
+    @Test
+    public void testAutoriserArmeAuxRetiaires() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#donnerUneArme(Integer,Integer)
+     */
+    @Test
+    public void testDonnerUneArme() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerArmes()
+     */
+    @Test
+    public void testListerArmes() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerArmesDispoMirmillon()
+     */
+    @Test
+    public void testListerArmesDispoMirmillon() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerArmesDispoRetiaire()
+     */
+    @Test
+    public void testListerArmesDispoRetiaire() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#decrireArme(Integer)
+     */
+    @Test
+    public void testDecrireArme() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#nomDeLArme(Integer)
+     */
+    @Test
+    public void testNomDeLArme() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#creerEthnie(String)
+     */
+    @Test
+    public void testCreerEthnie() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerEthnies()
+     */
+    @Test
+    public void testListerEthnies() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#listerGladiateursDEthnie(Integer)
+     */
+    @Test
+    public void testListerGladiateursDEthnie() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#decrireEthnie(Integer)
+     */
+    @Test
+    public void testDecrireEthnie() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#getScore(Integer)
+     */
+    @Test
+    public void testGetScore() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#frapper(Integer,Integer,Integer)
+     */
+    @Test
+    public void testFrapper() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#desarmer(Integer,Integer)
+     */
+    @Test
+    public void testDesarmer() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#vainqueurs()
+     */
+    @Test
+    public void testVainqueurs() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#nomDuGladiateur(Integer)
+     */
+    @Test
+    public void testNomDuGladiateur() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see Facade#nomDeLEthnie(Integer)
+     */
+    @Test
+    public void testNomDeLEthnie() {
+        fail("Unimplemented");
+    }
+}
